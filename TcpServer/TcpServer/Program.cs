940cd8daac4c263615f47c28e6771e11921de8c5
PortedUser <144102539+PortedUser@users.noreply.github.com>
2024-04-26 12:24:33 +0300

    Simple Server Func

--- TcpServer/TcpServer/Program.cs
+++ TcpServer/TcpServer/Program.cs
@@ -1,31 +1,77 @@
-﻿using System;
-using System.Text;
-using System.Net;
+﻿using System.Net;
 using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Text;
 
-class Program
+IPAddress adr = IPAddress.Parse("127.0.0.1");
+var tcpListener = new TcpListener(adr, 4001);
+Console.WriteLine(adr);
+
+try
 {
-    static void Main()
+    tcpListener.Start();  
+    Console.WriteLine("Сервер запущен. Ожидание подключений... ");
+
+    while (true)
     {
-        IPAddress adr = IPAddress.Parse("127.0.0.1");
-        TcpListener server = new TcpListener(adr, 4001);
-        server.Start();
-        Console.WriteLine(adr);
+        var tcpClient = await tcpListener.AcceptTcpClientAsync();
 
-        while (true)
-        {
-            TcpClient client = server.AcceptTcpClient();
-            NetworkStream ns = client.GetStream();
-            
-            var msg = Encoding.ASCII.GetBytes("1000");
-
-            while (client.Connected)
-            {
-                //int count = ns.Read(msg, 0, msg.Length);
-                ns.Write(msg,0, msg.Length );
-                Console.WriteLine(msg.Length);
-                Console.WriteLine("Connect");
-            }
-        }
+        await Task.Run(async () => await ProcessClientAsync(tcpClient));
     }
 }
+finally
+{
+    tcpListener.Stop();
+}
+
+
+
+async Task ProcessClientAsync(TcpClient tcpClient)
+{
+    
+    
+    var stream = tcpClient.GetStream();
+    var count = 0;
+
+
+    while (true)
+    {
+        count++;
+        try
+        {
+            await stream.WriteAsync(Encoding.UTF8.GetBytes(GetCorrectString(count, 4)));
+            Task.Delay(100);
+        }
+        catch (Exception)
+        {
+
+            break;
+        }
+        if (count >=99)
+        {
+            count = 0;
+        }
+    }
+    tcpClient.Close();
+}
+
+string GetCorrectString(int number, int length)
+{
+    var res = number.ToString();
+
+    if (res.Length < length)
+    {
+        var stab = length - res.Length;
+        var builder = new StringBuilder();
+
+        for (int i = stab; i > 0; i--)
+        {
+            builder.Append("0");
+        }
+        builder.Append(res);
+        res = builder.ToString();
+    }
+
+    return res;
+}
+
