30e4827c18c2ccd4c0db99766602bb56162bc810
PortedUser <144102539+PortedUser@users.noreply.github.com>
2024-04-26 08:39:43 +0300

    Folder layout

---
+++ TcpServer/TcpServer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+class Program
+{
+    static void Main()
+    {
+        IPAddress adr = IPAddress.Parse("127.0.0.1");
+        TcpListener server = new TcpListener(adr, 4001);
+        server.Start();
+        Console.WriteLine(adr);
+
+        while (true)
+        {
+            TcpClient client = server.AcceptTcpClient();
+            NetworkStream ns = client.GetStream();
+            
+            var msg = Encoding.ASCII.GetBytes("1000");
+
+            while (client.Connected)
+            {
+                //int count = ns.Read(msg, 0, msg.Length);
+                ns.Write(msg,0, msg.Length );
+                Console.WriteLine(msg.Length);
+                Console.WriteLine("Connect");
+            }
+        }
+    }
+}
